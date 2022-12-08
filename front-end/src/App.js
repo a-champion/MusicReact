54414f1a9169ba27a04688940a3cecfb107a8029
a-champion <a.champion1506@gmail.com>
2022-12-08 12:48:24 -0700

    working on albums component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Albums from './components/Albums';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello MusicReact</h1>
+      <Albums />
     </div>
   );
 }
