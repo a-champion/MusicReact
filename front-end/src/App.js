e91d24220a76206e41a4610d25015dcebd5a7a28
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-08 13:08:51 -0600

    added albums hook to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 const App = () => {
+const  [albums, setAlbums] = useState([])
+
+
+
   return (
     <div className="App">
       <h1>Hello MusicReact</h1>
+      <input type="text" onChange={handleAlbumSearch}/> 
     </div>
   );
 }
