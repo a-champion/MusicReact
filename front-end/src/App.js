f09a61458b65898ccdb72748f178374488b98206
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-08 13:38:36 -0600

    new form component set up and works. use effect on app.js set up, pupulates localhost:3000/albums with response data thru hook of albums

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,16 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import NewForm from './components/newalbum'
 
 const App = () => {
 const  [albums, setAlbums] = useState([])
 
-
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/albums')
+      .then((response)=> {
+        setAlbums(response.data)
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1>Hello MusicReact</h1>
-      <input type="text" onChange={handleAlbumSearch}/> 
+     
+      <NewForm a/>
     </div>
   );
 }
